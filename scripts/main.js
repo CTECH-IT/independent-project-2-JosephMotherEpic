e9285341af08d9a9090c5b648d929eb5b0d9fb77
Joey Lindsay <61097657+TheJoaster@users.noreply.github.com>
2020-04-09 18:06:22 -0500

    added JS to change the class to change the icon

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-// Put your JavaScript here
+var menuButton = document.getElementById("menubutton");
+var menuIcon = document.getElementById("menuicon");
+var sideBar = document.getElementById("sidemenu");
+function openSideMenu(){
+    
+    menuIcon.setAttribute('class', 'fas fa-chevron-left');
+    menuButton.setAttribute('onclick', 'closeSideMenu();');
+}
+function closeSideMenu(){
+    menuIcon.setAttribute('class', 'fas fa-chevron-right');
+    menuButton.setAttribute('onclick', 'openSideMenu();');
+}

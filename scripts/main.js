8578a19603fb3266a43021795390b8b82c9fea23
JosephMotherEpic <7535101@isd535.org>
2020-04-13 17:42:02 -0500

    tried adding sidebar

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,26 @@
-var menuButton = document.getElementById("menubutton");
-var menuIcon = document.getElementById("menuicon");
-var sideBar = document.getElementById("sidemenu");
-function openSideMenu(){
-    
-    menuIcon.setAttribute('class', 'fas fa-chevron-left');
-    menuButton.setAttribute('onclick', 'closeSideMenu();');
+let sidebar = document.getElementById("sidebar");
+let btnIcon = document.getElementById("btnicon");
+let button = document.getElementById("menubutton");
+let textElements = document.getElementsByClassName("text");
+function openSidebar(){
+    button.setAttribute('onclick','closeSidebar();');
+    btnIcon.setAttribute('class', 'fas fa-chevron-left');
+    showText();
 }
-function closeSideMenu(){
-    menuIcon.setAttribute('class', 'fas fa-chevron-right');
-    menuButton.setAttribute('onclick', 'openSideMenu();');
+function closeSidebar(){
+    button.setAttribute('onclick','openSidebar();');
+    btnIcon.setAttribute('class','fas fa-chevron-right');
+    hideText();
+}
+function showText(){
+    sidebar.setAttribute('class','expand');
+    for (i=0; i < textElements.length; i++){
+        textElements[i].setAttribute('class','text textopen');
+    }
+}
+function hideText(){
+    sidebar.setAttribute('class','shrink');
+    for (i=0; i < textElements.length; i++){
+        textElements[i].setAttribute('class','text textclosed');
+    }
 }
